f01f1756fb05e24bd0a8c5854a9f2f174a1e7f2a
Oyindamola Subair <oyindamolasubair@Oyindamolas-MacBook-Pro.local>
2017-08-29 12:23:03 +0100

    ft(recipe): integrate webpack eslint codeclimate react devtool

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: [
+        'babel-polyfill',
+        'webpack-dev-server/client?http://localhost:5000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'src/index.tpl.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        new webpack.NamedModulesPlugin()
+    ],
+    module: {
+        rules: [
+          {
+            enforce: 'pre',
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'eslint-loader'
+        },
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
+            },
+            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file-loader' }
+        ]
+    }
+};
